629b411370aeb2cd0244ddcfb2f5869a380a87c0
cesar-salcedo <cesarsalcedogarcia@gmail.com>
2025-06-23 12:51:46 +0200

    Con descripcion de boca

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,11 +9,39 @@ import img5 from '../assets/images/mouth_05.jpg';
 
 export default function Portfolio() {
     return (
-        <div>
+        <div style={{
+            marginTop: "32px"
+        }}>
+            <h1 style={{
+                textAlign: "center",
+                marginBottom: "64px",
+                color: "black"
+            }}>PORTFOLIO</h1>
+            <h2 style={{
+                marginTop: "32px",
+                textAlign: "center",
+                color: "#25a9f0"
+            }}>Human Mouth</h2>
+            <h4 style={{
+                marginTop: "8px",
+                textAlign: "center",
+                marginBottom: "16px",
+
+            }}>Optimized for close-up rendering and facial animation. Features 8K textures, 16-bit displacement maps, and full compatibility with Cycles and V-Ray. Includes original modeling, UV layout, shading, rigging, and presentation.
+
+            </h4>
             <PortfolioItem
 
                 images={[img1, img2, img3, img4, img5]}
             />
+            <h4 style={{
+                marginTop: "8px",
+                textAlign: "center",
+                marginBottom: "16px",
+            }}>$21.000 profit. + 300 sales.
+
+            </h4>
+
             {/* Puedes repetir para más proyectos */}
         </div>
     );
